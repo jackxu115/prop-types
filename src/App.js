666761986042edf1f66377a236fc1d99e746425b
pengxu <pengxu19890115@gmail.com>
2019-11-21 11:14:20 -0800

    propTypes and default props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Summary = ({
+  ingredients = 9,
+  steps = 2,
+  title = '[untitled recipe]'
+}) => (
+  <div className="summary">
+    <h1>{title}</h1>
+    <p>
+      <span>{ingredients} Ingredients | </span>
+      <span>{steps} Steps</span>
+    </p>
+  </div>
+);
+
+Summary.propTypes = {
+  ingredients: PropTypes.number,
+  steps: PropTypes.number,
+  title: (props, propName) =>
+    typeof props[propName] !== 'string'
+      ? new Error('A title must be a string')
+      : props[propName].length > 20
+      ? new Error('title is over 20 characters')
+      : null
+};
+
+const App = () => <Summary title={'Apple Juice'} />;
 
 export default App;
